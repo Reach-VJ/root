d585fd45c99980dfb2fadedd1c92b38d7a70c94e
Reach-VJ <66485684+Reach-VJ@users.noreply.github.com>
2020-10-17 19:09:36 -0700

    Update KotlinExample.kt

--- Kotlin/src/main/kotlin/KotlinExample.kt
+++ Kotlin/src/main/kotlin/KotlinExample.kt
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
+
 fun main() {
-    println("Hello Kotlin, Welcome to new programing language")
-}
-=======
-fun main() {
-  println("Welcome to new programming language, Kotlin123")
-}
->>>>>>> 71c46ac973cc60ecf99e2e7c00cbb11c409faa27
+  println("Welcome to new programming language, Kotlin")
+
