0c986e34c695a00705896b37cbf9818f51ab7b0d
Reach-VJ <66485684+Reach-VJ@users.noreply.github.com>
2020-10-17 19:37:34 -0700

    Update KotlinExample.kt

--- Kotlin/src/main/kotlin/KotlinExample.kt
+++ Kotlin/src/main/kotlin/KotlinExample.kt
@@ -1,9 +1,3 @@
 fun main() {
-<<<<<<< HEAD
   println("Welcome to new programming language, Kotlin!")
 }
-
-=======
-  println("Welcome to new programming language, Kotlin")
-}
->>>>>>> local_branch
